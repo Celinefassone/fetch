4a71a0e7409bc674ccfabf6ed9ec356be33dc196
Celine Fassone <celinefassone@MacBook-Air-di-Celine.local>
2023-02-23 15:27:49 +0100

    we can change the tab!

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
   // create a new XMLHttpRequest object
   var xhr = new XMLHttpRequest();
   // set the HTTP method and URL of the request
-  xhr.open("GET", "http://api.tvmaze.com/search/shows?q=batman");
+  xhr.open("GET", "https://api.tvmaze.com/search/shows?q=batman");
   // set the response type
   xhr.responseType = "json";
   // set up a function to handle the response
@@ -17,4 +17,29 @@ document.addEventListener("DOMContentLoaded", (e) => {
   };
   // send the request
   xhr.send();
+});
+const clearClasses = function (iterator) {
+  iterator.forEach(function (element) {
+    element.classList.remove("active");
+  });
+};
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const tabs = document.querySelectorAll(".tab-name");
+  const tabContentContainers = document.querySelectorAll(".tab-content");
+
+  tabs.forEach(function (tab) {
+    tab.addEventListener("click", function (event) {
+      clearClasses(tabs);
+      clearClasses(tabContentContainers);
+
+      const contentElement = document.querySelector(
+        `[data-content="${this.dataset.tab}"]`
+      );
+
+      contentElement.classList.add("active");
+
+      this.classList.add("active");
+    });
+  });
 });
