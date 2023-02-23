41d9ee0318101b4795c7f1ccd908ec6e52438d6f
Digital Knight <digitalknight@Digitals-MacBook-Pro.local>
2023-02-23 16:46:27 +0100

    Do some more ajax stuff

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,50 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  // create a new XMLHttpRequest object
+const defaultShows = "cars";
+
+const createRequest = function (search) {
+  search = search || defaultShows;
   var xhr = new XMLHttpRequest();
-  // set the HTTP method and URL of the request
-  xhr.open("GET", "https://api.tvmaze.com/search/shows?q=batman");
-  // set the response type
+  xhr.open("GET", `https://api.tvmaze.com/search/shows?q=${search}`);
   xhr.responseType = "json";
-  // set up a function to handle the response
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      // the request was successful
-      console.log(xhr.response);
-    } else {
-      // there was an error with the request
-      console.log("Error: " + xhr.status);
-    }
-  };
-  // send the request
-  xhr.send();
-});
+
+  return xhr;
+};
+
+const renderList = function (film) {
+  const element = document.createElement("li");
+  element.classList.add("list");
+  element.innerHTML = `
+    <a href="/preview.html">
+      <img class="list-img" src="${film.show.image.original}" />
+    </a>
+    <p class="list-text">${film.show.name}</p>
+    <p class="list-text">${
+      film.show.rating.average
+        ? "Rating " + film.show.rating.average
+        : "Unrated"
+    }</p>
+  `;
+
+  return element;
+};
+
+const renderTile = function (film) {
+  const element = document.createElement("div");
+  element.innerHTML = `
+  <a href="/preview.html">
+    <div
+      class="grid"
+      style="background-image: url('${film.show.image.original}');"
+    >
+      <p class="grid-text">Title: ${film.show.name} | ${
+    film.show.rating.average ? "Rating " + film.show.rating.average : "Unrated"
+  }</p>
+    </div>
+  </a>
+  `;
+
+  return element;
+};
+
 const clearClasses = function (iterator) {
   iterator.forEach(function (element) {
     element.classList.remove("active");
@@ -25,8 +52,29 @@ const clearClasses = function (iterator) {
 };
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  const listBoxElement = document.querySelector("#listbox");
+  const tilesContainer = document.querySelector("#tiles-container");
   const tabs = document.querySelectorAll(".tab-name");
   const tabContentContainers = document.querySelectorAll(".tab-content");
+  // const searchElement = document.querySelectorAll("#celines-search-input");
+  // const submitButton = document.querySelectorAll("#celines-button");
+
+  let xhr;
+  xhr = createRequest();
+
+  xhr.onload = function () {
+    xhr.response.forEach(function (film) {
+      listBoxElement.appendChild(renderList(film));
+      tilesContainer.appendChild(renderTile(film));
+    });
+  };
+
+  // submitButton.addEventListener('click', function () {
+  // xhr = createRequest(pass it in here the value of the input);
+  // xhr.send();
+  // })
+
+  xhr.send();
 
   tabs.forEach(function (tab) {
     tab.addEventListener("click", function (event) {
