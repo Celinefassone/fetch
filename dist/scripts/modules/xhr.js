6e4e6f882b9147da064808e7388d9a55319e1423
Celine Fassone <celinefassone@MacBook-Air-di-Celine.local>
2023-03-09 16:55:14 +0100

    modules are ready

--- dist/scripts/modules/xhr.js
+++ dist/scripts/modules/xhr.js
@@ -1,17 +1,7 @@
-export const createRequest = function () {
-  const defaultShows = "batman";
-  const idString = window.location.search;
-  const idValue = idString.split("=")[1];
+export const createRequest = function (id) {
+  const url = id ? `/lookup/shows?thetvdb=${id}` : 'search/shows?q=batman'
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", `https://api.tvmaze.com/search/shows?q=${defaultShows}`);
+  xhr.open("GET", `https://api.tvmaze.com/${url}`);
   xhr.responseType = "json";
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log(xhr.response);
-    } else {
-      console.log("Error: " + xhr.status);
-    }
-  };
-
   return xhr;
 };
